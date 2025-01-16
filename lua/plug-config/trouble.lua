7efc200fd560e2c50e786d605cc591752388fc15
luked <locdaohuuit@outlook.com>
2025-01-15 22:42:14 -0800

    fix trouble

--- lua/plug-config/trouble.lua
+++ lua/plug-config/trouble.lua
@@ -1,14 +1,11 @@
-local status_ok, zen = pcall(require, "trouble")
+local status_ok, trouble = pcall(require, "trouble")
 if not status_ok then
     return
 end
-
+trouble.setup()
 local map = require("lib.core").map
 
 
-map("n", "<leader>xx", function() require("trouble").toggle() end)
-map("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-map("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-map("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-map("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-map("n", "<leader>gR", function() require("trouble").toggle("lsp_references") end)
+map("n", "<leader>xx", ":Trouble diagnostics toggle<CR>")
+map("n", "<leader>xX", ":Trouble diagnostics toggle filter.buf=0<CR>")
+map("n", "<leader>xq", "Trouble qflist toggle<cr>")
